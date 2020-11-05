423de3c49e03a52be496949c332732845f907047
Shubhadarshie Nanda <50710277+nanda-mik@users.noreply.github.com>
2020-11-05 13:02:25 +0530

    zoom feature with issues

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 import Whiteboard from './components/Whiteboard/whiteboard';
 import Toolbar from './components/Toolbar/toolbar';
 
@@ -19,10 +20,26 @@ export default function App() {
 
 
   return (
-    <div className="App">
-      <Toolbar onChoice={onClickHandler} />
-      <Whiteboard choice={choice} />
-    </div>
+    <TransformWrapper
+      defaultScale={1}
+      defaultPositionX={200}
+      defaultPositionY={100}
+    >
+      {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
+        <React.Fragment>
+          <Toolbar onChoice={onClickHandler} />
+          <TransformComponent>
+            <div>Hell react sketch</div>
+            {/* <div><Whiteboard choice={choice} /></div> 
+            while using this i get this error Maximum update depth exceeded.*/}
+          </TransformComponent>
+          <div className="tools">
+            <button onClick={zoomIn}>+</button>
+            <button onClick={zoomOut}>-</button>
+            <button onClick={resetTransform}>x</button>
+          </div>
+        </React.Fragment>
+      )}
+    </TransformWrapper>
   );
 }
-
